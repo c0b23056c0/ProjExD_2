8938316b9b3d9ebf2e3e7890cc097817eee66bd7
Takuya <c0b23056c0@edu.teu.ac.jp>
2024-04-23 15:00:11 +0900

    れんしゅう３：たてと　

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,16 +13,16 @@ dic_mv = {#押下キーと移動量の辞書
         pg.K_RIGHT:(+5, 0)}
 
 
-def check_bound(obj_rct:pg.Rect):
+def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
     """
     こうかとんRect,または爆弾Rectの画面外判定用の関数
     引数：こうかとんRect,または爆弾Rect
     戻り値：横方向判定結果，縦方向判定結果（True:画面内，False:画面外）
     """
     yoko, tate = True, True
-    if obj_rct.right < 0 or WIDTH < obj_rct.left:
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
         yoko = False
-    if obj_rct.bottom < 0 or HEIGHT < obj_rct.top:
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
     return yoko, tate
 
@@ -30,10 +30,12 @@ def check_bound(obj_rct:pg.Rect):
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    #こうかとんの設定
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    #爆弾の設定
     bom_img = pg.Surface((20, 20))
     bom_img.set_colorkey((0, 0, 0))
     pg.draw.circle(bom_img,(255, 0, 0), (10, 10), 10)
@@ -50,6 +52,7 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        #こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k,v in dic_mv.items():
@@ -57,9 +60,17 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        #爆弾の移動と表示
         bom_rct.move_ip(vx,vy)
         screen.blit(bom_img, bom_rct)
+        yoko, tate = check_bound(bom_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         pg.display.update()
         tmr += 1
         clock.tick(50)
