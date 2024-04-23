ac5f2d1c48e89f2d8930f0776e1e4da591056478
Takuya <c0b23056c0@edu.teu.ac.jp>
2024-04-23 17:06:22 +0900

    gameoverと

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+import time
 import pygame as pg
 
 
@@ -25,6 +26,54 @@ def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
     if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
     return yoko, tate
+
+
+def kk_rt(img, idouryou):
+    """
+    こうかとんの回転角度計算用の関数
+    引数：kk_imgと移動量のタプル
+    戻り値：移動量の合計値タプルのキーとrotozoomしたSurfaceを値とした辞書
+    """
+    if idouryou == (-5, 0):
+        return {idouryou:pg.transform.rotozoom(img, 0, 1.0)}
+    if idouryou == (-5, +5):
+        return {idouryou:pg.transform.rotozoom(img, 45, 1.0)}
+    if idouryou == (-5, -5):
+        return {idouryou:pg.transform.rotozoom(img, 315, 1.0)}
+    if idouryou == (0, +5):
+        pg.transform.flip(img, True, False)
+        return {idouryou:pg.transform.rotozoom(img, 90, 1.0)}
+    if idouryou == (+5, +5):
+        pg.transform.flip(img, True, False)
+        return {idouryou:pg.transform.rotozoom(img, 45, 1.0)}
+    if idouryou == (0,-5):
+        pg.transform.flip(img, True, False)
+        return {idouryou:pg.transform.rotozoom(img, 270, 1.0)}
+    if idouryou == (+5, -5):
+        pg.transform.flip(img, True, False)
+        return {idouryou:pg.transform.rotozoom(img, 315, 1.0)}
+    if idouryou == (+5, 0):
+        return {idouryou:pg.transform.rotozoom(img, 0, 1.0)}
+
+
+def gameover(screen):
+    go_img = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(go_img,(0,0, 0), (0, 0, WIDTH, HEIGHT), width = 0)
+    go_rct = go_img.get_rect()
+    go_img.set_alpha(200)
+    screen.blit(go_img, go_rct)
+    fonto = pg.font.Font(None, 100)
+    txt = fonto.render("Game Over", True, (255, 255, 255))
+    screen.blit(txt,[600, 400])
+    koka_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 2.0)
+    koka_rct = koka_img.get_rect()
+    koka_rct.center = 500, 450
+    screen.blit(koka_img, koka_rct)
+    koka_rct.center = 1100, 450
+    screen.blit(koka_img, koka_rct)
+    pg.display.update()
+    time.sleep(5)
+
 
 
 def main():
@@ -52,20 +101,32 @@ def main():
                 return
         if kk_rct.colliderect(bom_rct): # こうかとんと爆弾がぶつかったら
             print("Game Over")
+            gameover(screen)
             return
         screen.blit(bg_img, [0, 0]) 
 
         #こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+        kk_new_img = pg.transform.rotozoom(kk_img, 0, 1.0)
         for k,v in dic_mv.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+                new_v = (v[0], v[1])
+                print(new_v)
+                dic_rtz = kk_rt(kk_img,new_v)
+                kk_new_img = dic_rtz[new_v]
+                print(dic_rtz)
+            """
+            pi = kk_rt(k)
+            print(pi)
+            kk_img = pg.transform.rotate(kk_img, pi)
+            """
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)
+        screen.blit(kk_new_img, kk_rct)
         #爆弾の移動と表示
         bom_rct.move_ip(vx,vy)
         screen.blit(bom_img, bom_rct)
